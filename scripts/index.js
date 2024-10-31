f568cf5bcfc49dd98b0bc86aaf91fd89b6dd0c94
DevinneLeigh <159208726+DevinneLeigh@users.noreply.github.com>
2024-10-31 14:42:39 -0600

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -3,3 +3,8 @@
 let  grid = document.querySelectorAll(".grid")
 let input = document.querySelector("#user_input")
 let submit = document.querySelector("#submit")
+
+function hiddenFunction(event) {
+    document.getElementById("next").style.display = "block";
+  }
+
