7355be39562848466cd52ceb708a762a90c36272
Jacob Brown <Jacob_E_Brown@protonmail.com>
2024-11-20 13:24:41 -0700

    Added a Code Mirror Editor to the Game Site. The CSS is a little screwy and I hope someone can fix it.

---
+++ rollup.config.mjs
@@ -0,0 +1,15 @@
+// This file is used to compile the cloud mirror code editor and ourput the editor as editor.bundle.js.
+// to modify the editor, add code mirror plugins to editor.mjs and run rollup -c (make sure it is installed on your system with npm)
+// Source: https://codemirror.net/examples/bundle/
+
+
+
+import { nodeResolve } from "@rollup/plugin-node-resolve"
+export default {
+    input: "./scripts/editor.mjs",
+    output: {
+        file: "./scripts/editor.bundle.js",
+        format: "iife"
+    },
+    plugins: [nodeResolve()]
+}
